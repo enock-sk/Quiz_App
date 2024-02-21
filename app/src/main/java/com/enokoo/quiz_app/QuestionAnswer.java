aebc4b022586c94babb5e2ad041483f251195a6e
enock-sk <ekiplangat64@gmail.com>
2024-02-21 17:02:19 +0300

    Android question app

---
+++ app/src/main/java/com/enokoo/quiz_app/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.enokoo.quiz_app;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Who developed the Linux kernel?",
+"Who is known as the father of the computer?",
+            "Who co-founded Apple Inc?",
+            "Who invented the World Wide Web?",
+            "Who is considered one of the fathers of artificial intelligence?",
+
+    };
+    public static String choices[][]={
+            {"Steve Wozniak","Linus Torvalds","Richard Stallman","Ken Thompson"},
+            {"Charles Babbage","Alan Turing","John von Neumann","Bill Gates"},
+            {"Mark Zuckerberg","Larry Page","Steve Jobs "," Michael Dell"},
+            {"Tim Berners-Lee","Vint Cerf","Marc Andreessen","Bob Kahn"},
+            {"Elon Musk","John McCarthy","Geoffrey Hinton","Andrew Ng"},
+    };
+    public static String correctAnswers[]={
+            "Linus Torvalds",
+            "Charles Babbage",
+            "Steve Jobs",
+            "Tim Berners-Lee",
+            "John McCarthy"
+    };
+}
